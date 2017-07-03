94e159d99af70428f7f620c6745b2dead4969eec
Kim Durante <kimberlydurante@gmail.com>
2017-07-03 15:12:27 -0700

    Create addMetadata.py

---
+++ ISO/addMetadata.py
@@ -0,0 +1,62 @@
+#This script reads values from a csv and inserts them into the appropriate metadata fields.
+#in this example, a csv named 'identifiers.csv' contains 3 columns: filenames, unique ids ('identifiers'), and the title for each layer
+
+import csv
+import os
+import xml.etree.ElementTree as ET
+namespaces = {'gmd': 'http://www.isotc211.org/2005/gmd','gco': 'http://www.isotc211.org/2005/gco', 'gml': 'http://www.opengis.net/gml', 'gfc': 'http://www.isotc211.org/2005/gfc'}
+
+metadict = {}
+reader = csv.reader(open("shapefiles.csv", "rU"))
+for rows in reader:
+    filename = rows[0]
+    identifier = rows[1]
+    layerTitle = rows[2]
+    metadict[filename] = identifier, layerTitle
+
+def createMetadata():
+    print f
+    for k, v in metadict.items():
+        if v[1] == title.text:
+           metadataID.text = 'DOGS' + v[0]
+           URL.text = 'http//purl.stanford.edu/' + v[0]
+           distURL.text = 'http//purl.stanford.edu/' + v[0]
+           distName.text = k
+
+def createCitation():
+    Originator = 'AUTHOR(S). '
+    Publisher = 'PUBLISHER'
+    Date = date.text
+    credit.text = Originator + '(' + Date[:4] + '). ' + title.text + '. ' + Publisher + '. Available at: ' + URL.text + '.'
+    print credit.text
+
+
+for dirName, subDirs, fileNames in os.walk('.'):
+    for f in fileNames:
+        if f.endswith('.xml'):
+            file = os.path.join(dirName, f)
+            tree = ET.parse(file)
+            root = tree.getroot()
+            ET.register_namespace('xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+            ET.register_namespace('gmd', 'http://www.isotc211.org/2005/gmd')
+            ET.register_namespace('gco', 'http://www.isotc211.org/2005/gco')
+            ET.register_namespace('gts', 'http://www.isotc211.org/2005/gts')
+            ET.register_namespace('gss', 'http://www.isotc211.org/2005/gss')
+            ET.register_namespace('gsr', 'http://www.isotc211.org/2005/gsr')
+            ET.register_namespace('gfc', 'http://www.isotc211.org/2005/gfc')
+            ET.register_namespace('gmx', 'http://www.isotc211.org/2005/gmx')
+            ET.register_namespace('gmi', 'http://www.isotc211.org/2005/gmi')
+            ET.register_namespace('gml', 'http://www.opengis.net/gml')
+            title = root.find('gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString', namespaces=namespaces)
+            date = root.find('gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:date/gmd:CI_Date/gmd:date/gco:Date', namespaces=namespaces)
+            credit = root.find('gmd:identificationInfo/gmd:MD_DataIdentification/gmd:credit/gco:CharacterString', namespaces=namespaces)
+            metadataID = root.find('gmd:fileIdentifier/gco:CharacterString', namespaces=namespaces)
+            URL = root.find('gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:identifier/gmd:MD_Identifier/gmd:code/gco:CharacterString', namespaces=namespaces)
+            URI = root.find('gmd:dataSetURI/gco:CharacterString', namespaces=namespaces)
+            distURL = root.find('gmd:distributionInfo/gmd:MD_Distribution/gmd:transferOptions/gmd:MD_DigitalTransferOptions/gmd:onLine/gmd:CI_OnlineResource/gmd:linkage/gmd:URL', namespaces=namespaces)
+            distName = root.find('gmd:distributionInfo/gmd:MD_Distribution/gmd:transferOptions/gmd:MD_DigitalTransferOptions/gmd:onLine/gmd:CI_OnlineResource/gmd:name/gco:CharacterString',  namespaces=namespaces)
+            createMetadata()
+            createCitation()
+ #           tree.write(file)
+            
+                
