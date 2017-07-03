0174491c7e5d391be641ef92e794fb5c9d3f321b
Kim Durante <kimberlydurante@gmail.com>
2017-07-03 14:51:34 -0700

    Update addAttrs.py

--- python/addAttrs.py
+++ python/addAttrs.py
@@ -1,17 +1,21 @@
 import csv
 import os
+import sys
 import xml.etree.ElementTree as ET
-namespaces = {'gmd': 'http://www.isotc211.org/2005/gmd','gco': 'http://www.isotc211.org/2005/gco', 'gml': 'http://www.opengis.net/gml', 'gfc': 'http://www.isotc211.org/2005/gfc'}
+namespaces = {'gmd': 'http://www.isotc211.org/2005/gmd','gco': 'http://www.isotc211.org/2005/gco', 'gml': 'http://www.opengis.net/gml', 'gmx':'http://www.isotc211.org/2005/gmx','gfc': 'http://www.isotc211.org/2005/gfc'}
 
-if __name__=="__main__":
-    attrdict = {}
-    reader = csv.reader(open("attributes.csv", "rU"))
-    for rows in reader:
-        label = rows[0]
-        definition = rows[1]
-        attrdict[label] = definition   
+attributes = {}
+reader = csv.reader(open("attributes.csv", "rU"))
+for rows in reader:
+    label = rows[0]
+    definition = rows[1]
+    attributes[label] = definition    
 
-tree = ET.parse('fcAGEB.xml')
+in_file = sys.argv[-1]
+new_file = in_file[:-4] + '_fc.xml'
+
+open(new_file, 'w')
+tree = ET.parse('fc.xml')
 root = tree.getroot()
 ET.register_namespace('xsi', 'http://www.w3.org/2001/XMLSchema-instance')
 ET.register_namespace('gmd', 'http://www.isotc211.org/2005/gmd')
@@ -24,22 +28,32 @@ ET.register_namespace('gmx', 'http://www.isotc211.org/2005/gmx')
 ET.register_namespace('gmi', 'http://www.isotc211.org/2005/gmi')
 ET.register_namespace('gml', 'http://www.opengis.net/gml')
 featureType = root.find('gfc:featureType/gfc:FC_FeatureType', namespaces=namespaces)
-for k,v in attrdict.items():
-    featureType.insert(3,ET.Element('{http://www.isotc211.org/2005/gfc}carrierOfCharacteristics'))
-    featureAttribute = ET.SubElement(featureType[3],'{http://www.isotc211.org/2005/gfc}FC_FeatureAttribute')
-    memName = ET.SubElement(featureType[3][0],'{http://www.isotc211.org/2005/gfc}memberName')
+fc_title = root.find('gmx:name/gco:CharacterString', namespaces=namespaces)
+fc_date = root.find('gmx:versionDate/gco:Date', namespaces=namespaces)
+
+for k,v in attributes.items():
+    featureType.insert(4,ET.Element('{http://www.isotc211.org/2005/gfc}carrierOfCharacteristics'))
+    featureAttribute = ET.SubElement(featureType[4],'{http://www.isotc211.org/2005/gfc}FC_FeatureAttribute')
+    memName = ET.SubElement(featureType[4][0],'{http://www.isotc211.org/2005/gfc}memberName')
     locName = ET.SubElement(featureAttribute[0],'{http://www.isotc211.org/2005/gco}LocalName')
-    attr = ET.SubElement(featureType[3][0],'{http://www.isotc211.org/2005/gfc}definition')
-    definition = ET.SubElement(attr,'{http://www.isotc211.org/2005/gco}CharacterString')
-    cardinality = ET.SubElement(featureType[3][0],'{http://www.isotc211.org/2005/gfc}cardinality')
-    multiplicity = ET.SubElement(cardinality,'{http://www.isotc211.org/2005/gco}Multiplicity')
-    attrRange = ET.SubElement(multiplicity,'{http://www.isotc211.org/2005/gco}range')
-    mulRange = ET.SubElement(attrRange,'{http://www.isotc211.org/2005/gco}MultiplicityRange')
-    lowRange = ET.SubElement(mulRange,'{http://www.isotc211.org/2005/gco}lower')
-    lowRange.set('gco:nilReason', 'unknown')
-    upRange = ET.SubElement(mulRange,'{http://www.isotc211.org/2005/gco}upper')
-    upRange.set('gco:nilReason', 'unknown')
     locName.text = k
+    attr = ET.SubElement(featureType[4][0],'{http://www.isotc211.org/2005/gfc}definition')
+    definition = ET.SubElement(attr,'{http://www.isotc211.org/2005/gco}CharacterString')
     definition.text = v
-    print definition.text
-    tree.write('fcAGEB.xml')
+    cardinality = ET.SubElement(featureType[4][0],'{http://www.isotc211.org/2005/gfc}cardinality')
+    cardinality.set('gco:nilReason','unknown')
+
+
+
+
+
+##    multiplicity = ET.SubElement(cardinality,'{http://www.isotc211.org/2005/gco}Multiplicity')
+##    attrRange = ET.SubElement(multiplicity,'{http://www.isotc211.org/2005/gco}range')
+##    mulRange = ET.SubElement(attrRange,'{http://www.isotc211.org/2005/gco}MultiplicityRange')
+##    lowRange = ET.SubElement(mulRange,'{http://www.isotc211.org/2005/gco}lower')
+##    lowRange.set('gco:nilReason', 'unknown')
+##    upRange = ET.SubElement(mulRange,'{http://www.isotc211.org/2005/gco}upper')
+##    upRange.set('gco:nilReason', 'unknown')
+    
+ #   print (definition.text)
+tree.write(new_file)
