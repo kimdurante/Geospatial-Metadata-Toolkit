38b9304016bd409fc9731b4344c8a0d1a39deb16
Kim Durante <kimberlydurante@gmail.com>
2017-07-13 13:51:33 -0700

    Create addAttrs.py

---
+++ ArcGIS/addAttrs.py
@@ -0,0 +1,35 @@
+import csv
+import os
+import xml.etree.ElementTree as ET
+
+if __name__=="__main__":
+    attrdict = {}
+    reader = csv.reader(open("attrs.csv", "rU"))
+    for rows in reader:
+        label = rows[0]
+        definition = rows[1]
+        attrdict[label] = definition
+
+print attrdict
+for dirName, subDirs, fileNames in os.walk('.'):
+    for f in fileNames:
+        if f.endswith('.xml'):
+            file = os.path.join(dirName, f)
+            tree = ET.parse(file)
+            root = tree.getroot()
+            atts = root.findall('eainfo/detailed/attr')
+            alabel = root.findall('eainfo/detailed/attr/attrlabl')
+            ascale = root.findall('eainfo/detailed/attr/attscale')
+
+            for i in atts:
+                label = i[0].text
+                i.insert(6,ET.Element('attrdef'))
+                for k,v in attrdict.items():
+                    #print k, v
+                    if label == k:
+                       i[6].text = v
+                       print (label, i[6].text)
+  #                     tree.write(file)
+
+
+                
