06f9ddc21c0f872390b8d6459c1b723c3541bc78
Kim Durante <kimberlydurante@gmail.com>
2017-07-13 13:50:28 -0700

    Update addMetadata.py

--- ArcGIS/addMetadata.py
+++ ArcGIS/addMetadata.py
@@ -1,56 +1,57 @@
-##  This script scans a directory of shapefiles and TIFFs and prints out some data properties for each layer
-from osgeo import gdal, osr, ogr
+import csv
 import os
+import xml.etree.ElementTree as ET
 
+
+metadict = {}
+reader = csv.reader(open("metadata.csv", "rU"))
+for rows in reader:
+    filename = rows[0]
+    title = rows[1]
+    identifier = rows[2]
+    uuid = rows[3]
+    metadict[filename] = identifier, title
+
+originator = 'AUTHOR(S). '
+publisher = 'PUBLISHER'   
+
+def createMetadata():
+    print f
+    for k, v in metadict.items():
+        if v[1] == title:
+           metadataID.text = 'edu.stanford.purl:' + v[0]
+           URL.text = 'http//purl.stanford.edu/' + v[0]
+           distURL.text = 'http//purl.stanford.edu/' + v[0]
+           distName.text = k
+           URI.text = 'http//purl.stanford.edu/' + v[0]
+           fcTitle.text = 'Feature catalog for ' + ResTitle
+           fcID.text = uuid
+
+def createCitation():
+    credit.text = originator + '(' + pubDate + '). ' + title + '. ' + publisher + '. Available at: ' + URL.text + '.'
+
+    
 for dirName, subDirs, fileNames in os.walk('.'):
     for f in fileNames:
-        if f.endswith('.shp'):
-            print f
-            f = os.path.join(dirName, f)
-            size = os.path.getsize(f)
-            ds = ogr.Open(f)
-            for lyr in ds:
-                folder_size = 0
-                folder_size += os.path.getsize(f)
-                (minx, maxx, miny, maxy) = lyr.GetExtent()             
-                print("Geometry type: %s" % ogr.GeometryTypeToName(lyr.GetGeomType()))
-                srs = lyr.GetSpatialRef()
-                source = srs.ExportToXML()
-                print (f)
-                srsAuth = srs.GetAttrValue("AUTHORITY",0)
-                srsCode = srs.GetAttrValue("AUTHORITY",1)
-                if srsCode is not None:
-                   print ('Projection is: '+ srsAuth + '::' + srsCode)
-                else:
-                   print ('Projection is missing!')
-                 
-                #print ('Extent : %f, %f - %f %f' % (minx, miny, maxx, maxy)) #W-S-E-N
-                #print "Folder = %0.1f MB" % (folder_size/(1024*1024.0))
-                print ('\n')
-                                
+        if f.endswith("shp.xml"):
+            file = os.path.join(dirName, f)  
+            tree = ET.parse(file)
+            root = tree.getroot()
+            metadataID = root.find('mdFileID')
+            ResTitle = root.find('dataIdInfo/idCitation/resTitle').text
+            pubDate = root.find('dataIdInfo/idCitation/date/pubDate').text
+            pubDate = pubDate[:4]
+            distURL = root.find('distInfo/distTranOps/onLineSrc/linkage')
+            distName = root.find('distInfo/distTranOps/onLineSrc/orName')
+            URL = root.find('dataIdInfo/idCitation/citId/identCode')
+            fcID = root.find('contInfo/FetCatDesc/catCitation/citId/identCode')
+            URI = root.find('dataSetURI')
+            fcTitle = root.find ('contInfo/FetCatDesc/catCitation/resTitle')
+            credit = root.find('dataIdInfo/idCredit')
+            createMetadata()
+            createCitation()
 
-        if f.endswith ('.tif'):
-            print (f)
-            f = os.path.join(dirName, f)
-            ds = gdal.Open(f)
-            width = ds.RasterXSize
-            height = ds.RasterYSize
-            gt = ds.GetGeoTransform()
-            minx = gt[0]
-            miny = gt[3] + width*gt[4] + height*gt[5]
-            maxx = gt[0] + width*gt[1] + height*gt[2]
-            maxy = gt[3]
-            prj = ds.GetProjection()
-            srs=osr.SpatialReference(wkt=prj)
-            if srs.IsProjected:
-                srs = srs.GetAttrValue('authority', 0) + '::' + srs.GetAttrValue('authority', 1)
-                srsAuth =srs[4:]
-                srsCode =srs[:4]
-            print ('Raster Type: ', ds.GetDriver().ShortName,'/', \
-                  ds.GetDriver().LongName)
-            print ('Projection is: ' + srs)
-                  
-            #print ('Extent : %f, %f - %f %f' % (minx, miny, maxx, maxy))
-
-            print ('\n')
+            print (credit.text)
+  #         tree.write(file)
+                
 
