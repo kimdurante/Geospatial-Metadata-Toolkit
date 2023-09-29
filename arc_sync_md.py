43fcbf1f54b0d2a811e53f6d8b39c986adc4ac63
Kim Durante <kimberlydurante@gmail.com>
2023-09-29 10:56:07 -0700

    Create arc_sync_md.py
    ArcPy script to update metadata in ArcGIS

---
+++ arc_sync_md.py
@@ -0,0 +1,16 @@
+#Synchronize Metadata (arc_sync_md.py). Walk through a directory of shapefiles and GeoTIFFs, and update the metadata.
+import arcpy
+from arcpy import env
+from arcpy import metadata as md
+import os
+
+#Path to the data directory
+env.workspace = "W:\PATH_TO_DIRECTORY"
+
+for dirs, subdirs, files in os.walk(env.workspace):
+    for f in files:
+        if f.endswith(".shp") or f.endswith(".tif"):
+            filePath = os.path.join(dirs, f)
+            src_item_md = md.Metadata(filePath)
+            print (src_item_md)
+            src_item_md.synchronize("ALWAYS")
